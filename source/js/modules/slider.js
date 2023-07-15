c7f0a4d67620a92db4c140aa5ab4f18592e1e037
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2023-07-15 18:49:32 +0300

    feat(coach): добавляет и корректирует стили слайдера

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,12 +1,19 @@
 // eslint-disable-next-line no-undef
 const coachSlider = new Swiper('.coach__slider', {
+  initialSlide: 2,
   slidesPerView: 1,
   loop: true,
   breakpoints: {
     768: {
       slidesPerView: 2,
+      spaceBetween: 30,
     },
     1200: {
+      initialSlide: 0,
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1366: {
       initialSlide: 0,
       slidesPerView: 4,
       spaceBetween: 40,
