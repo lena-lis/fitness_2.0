bf4d2e6bee214b8528ab2eeef046ed8a3d934467
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2023-08-06 14:08:27 +0300

    fix(gym): дорабатывает скрипт

--- source/js/modules/play-video.js
+++ source/js/modules/play-video.js
@@ -1,11 +1,10 @@
 const initVideo = () => {
   const videoContainer = document.querySelector('[data-video-container]');
+  const playButton = videoContainer.querySelector('[data-video-button]');
+  const poster = videoContainer.querySelector('[data-video-poster]');
+  const player = videoContainer.querySelector('[data-video-player]');
 
-  if (videoContainer) {
-    const playButton = videoContainer.querySelector('[data-video-button]');
-    const poster = videoContainer.querySelector('[data-video-poster]');
-    const player = videoContainer.querySelector('[data-video-player]');
-
+  if (videoContainer && playButton && poster && player) {
     playButton.addEventListener('click', function (evt) {
       evt.preventDefault();
       poster.style.display = 'none';
