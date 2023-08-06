e9031e480b5ca6cb7473026d91fcaee1a37fb07a
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2023-08-06 13:58:12 +0300

    fix(gym): исправляет скрипт включения видео

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,8 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initTabs} from './modules/tabs/init-tabs.js';
 import {initAccordions} from './modules/accordions/init-accordion.js';
-import './modules/play-video.js';
+// import './modules/play-video.js';
+import {initVideo} from './modules/play-video.js';
 import './modules/slider.js';
 
 // ---------------------------------
@@ -20,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    initVideo();
     initTabs();
     initAccordions();
     const form = new Form();
--- source/js/modules/play-video.js
+++ source/js/modules/play-video.js
@@ -1,11 +1,18 @@
-const videoContainer = document.querySelector('.gym__video-container');
-const playButton = videoContainer.querySelector('.gym__video-button');
-const poster = videoContainer.querySelector('.gym__video-container img');
-const player = videoContainer.querySelector('.gym__video-container iframe');
+const initVideo = () => {
+  const videoContainer = document.querySelector('[data-video-container]');
 
-playButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  poster.style.display = 'none';
-  playButton.style.display = 'none';
-  player.src += '?autoplay=1';
-});
+  if (videoContainer) {
+    const playButton = videoContainer.querySelector('[data-video-button]');
+    const poster = videoContainer.querySelector('[data-video-poster]');
+    const player = videoContainer.querySelector('[data-video-player]');
+
+    playButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      poster.style.display = 'none';
+      playButton.style.display = 'none';
+      player.src += '?autoplay=1';
+    });
+  }
+};
+
+export {initVideo};
